cfe0af123a970d656349bf32d4433b40074acaec
zhangguang <zhangguang@drojian.dev>
2023-08-21 15:25:30 +0800

    1.1.6

--- Example/Pods/Target Support Files/TestPublicccc/TestPublicccc-umbrella.h
+++ Example/Pods/Target Support Files/TestPublicccc/TestPublicccc-umbrella.h
@@ -10,37 +10,7 @@
 #endif
 #endif
 
-#import "XYBasicViewController.h"
-#import "XYCustomNavigationBarView.h"
-#import "XYCustomTabbar.h"
-#import "XYCustomTabBarController.h"
-#import "XYDefineMacro.h"
-#import "XYNavigationController.h"
-#import "UIViewController+XYBasic.h"
-#import "XYCommonDefine.h"
-#import "XYAppControllers.h"
-#import "XYCustomButtonView.h"
-#import "XYCollectionCell.h"
-#import "XYCollectionHeaderCell.h"
-#import "XYEmptyCollectionHeaderFooterCell.h"
-#import "XYEmptyTableHeaderFooterCell.h"
-#import "XYTableHeaderCell.h"
-#import "XYWFCollectionCell.h"
-#import "XYCollectionReusableView.h"
-#import "XYCollectionView.h"
-#import "XYWaterFlowLayout.h"
-#import "XYWFCollectionView.h"
-#import "XYCellItem+ADD.h"
-#import "XYCellItem+Common.h"
-#import "XYCellItem.h"
-#import "XYActionSheetNavigationController.h"
-#import "XYActionSheetTransition.h"
-#import "XYAlertActionSheetViewController.h"
-#import "XYTableHeaderFooterView.h"
-#import "XYTableView.h"
-#import "XYTableViewCell.h"
 #import "TestViewController.h"
-#import "XYBasicClass.h"
 
 FOUNDATION_EXPORT double TestPubliccccVersionNumber;
 FOUNDATION_EXPORT const unsigned char TestPubliccccVersionString[];
--- TestPublicccc.podspec
+++ TestPublicccc.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'TestPublicccc'
-  s.version          = '1.1.5'
+  s.version          = '1.1.6'
   s.summary          = 'A short description of TestPublicccc.'
 #  s.resource = 'TestPublicccc/TestPublicccc.bundle'
 #  s.resource         = 'Assets.xcassets'
